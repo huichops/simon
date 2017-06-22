69e93f3057a332e327d920f741392560e227627c
Alex-TH <alex.trejoh@gmail.com>
2017-06-21 21:33:15 -0500

    Add test cases for game-state

---
+++ test/game-state.test.js
@@ -0,0 +1,51 @@
+import GameState from '../scripts/game-state';
+import constants from '../scripts/constants';
+
+const { BLINK, ADDING, LOST, PLAYING } = constants;
+
+const inscreaseScore = (state, times) => {
+  for(let i = 0; i < times; i += 1) state.increaseScore();
+};
+
+test('Try to increase score on BLINK status', () => {
+  const state = new GameState();
+  state.increaseScore();
+  expect(state.getState().score).toBe(0);
+});
+
+test('Increase score on PLAYING status', () => {
+  const state = new GameState();
+  state.changeStatus(PLAYING);
+  inscreaseScore(state, 3);
+  expect(state.getState().score).toBe(3);
+});
+
+test('Reset score should set hiScore', () => {
+  const state = new GameState();
+  state.changeStatus(PLAYING);
+  inscreaseScore(state, 3);
+  state.resetGame();
+  expect(state.getState().hiScore).toBe(3);
+});
+
+test('hiScore should be preserved', () => {
+  const state = new GameState();
+  state.changeStatus(PLAYING);
+  inscreaseScore(state, 3);
+  state.resetGame();
+  state.changeStatus(PLAYING);
+  inscreaseScore(state, 2);
+  state.resetGame();
+  expect(state.getState().hiScore).toBe(3);
+});
+
+test('hiScore should be preserved', () => {
+  const state = new GameState();
+  state.changeStatus(PLAYING);
+  inscreaseScore(state, 3);
+  state.resetGame();
+  state.changeStatus(PLAYING);
+  inscreaseScore(state, 4);
+  state.resetGame();
+  expect(state.getState().hiScore).toBe(4);
+});
